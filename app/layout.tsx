acd074263b4984008fbc31b56a46ca3e6494e787
Samy Rahim <102865808+Samio735@users.noreply.github.com>
2024-01-24 15:13:46 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type  Metadata  from "next";
-import  Poppins  from "next/font/google";
+import  {Poppins}  from "next/font/google";
 import NavBar from "../components/NavBar";
 
 // Font files can be colocated inside of `app`
